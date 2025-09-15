ead0eafff1837e03f34d9b2bac311f151602e0cd
Benjamín Peralta <benjaminp158@gmail.com>
2025-09-14 23:07:12 -0300

    refactor: cambiado el Autoload data por un resource

--- savedata/player_data_resource.gd
+++ savedata/player_data_resource.gd
@@ -3,7 +3,9 @@ extends Node
 const human := "SAMUEL"
 var lv := 20
 var hp := 90
-var items := ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 5", "Item 5", "Item 5"]
+var items: Array[String] = [
+	"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 5", "Item 5", "Item 5"
+]
 
 var weapon := "Real Knife"
 var armor: String
