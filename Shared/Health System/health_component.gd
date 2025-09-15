d12c25f39397347bed464e7e1587d709098ee3ec
Benjamín Peralta <benjaminp158@gmail.com>
2025-09-14 23:25:25 -0300

    refactor: to keep nice access methods

--- Shared/Health System/health_component.gd
+++ Shared/Health System/health_component.gd
@@ -1,35 +1,41 @@
 class_name HealthComponent
 extends Node
 
-signal damaged
+signal damaged(amount: int)
 signal max_health_incremented
+signal healed(amount: int)
 signal died
-signal dying
 
-@export var max_health_limit: int = 100
 @export var max_health: int:
 	set(value):
-		max_health = value if value <= max_health_limit else max_health_limit
+		max_health = value if value > 0 else 1
 var health: int:
-	set(value):
-		health = value if value >= 0 else 0
+	get:
+		return _health
+var _health: int
 
 
 func _ready() -> void:
-	health = max_health
+	_health = max_health
 
 
-func inscrease_max_health() -> void:
-	max_health += 1
+func inscrease_max_health(amount: int) -> void:
+	max_health += amount
 	max_health_incremented.emit()
 
 
-func harm(value: int) -> void:
-	health -= value
-	damaged.emit()
+func harm(amount: int) -> void:
+	_health -= amount
+	if _health < 0:
+		_health = 0
+	damaged.emit(amount)
 
-	if health == 0:
-		dying.emit()
-		await get_tree().create_timer(0.5).timeout
+	if _health == 0:
 		died.emit()
-		get_parent().queue_free()
+
+
+func heal(amount: int) -> void:
+	_health += amount
+	if _health > max_health:
+		_health = max_health
+	healed.emit(amount)
